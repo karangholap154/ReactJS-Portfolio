181d77826a0b3d1d8d14df6f92eb9f868f804383
karann001 <karangholap154@gmail.com>
2025-02-05 23:51:40 +0530

    added home and about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/header/header';
+import { Home } from './components/home/Home';
+import { About } from './components/about/About';
 
 const App = () => {
   return (
     <>
     <Header />
+    <main className='main'>
+      <Home />
+      <About />
+    </main>
     </>
   );
 }
