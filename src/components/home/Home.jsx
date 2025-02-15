7735f385f651a4ab2646324f732495782337c095
karann001 <karangholap154@gmail.com>
2025-02-15 22:59:40 +0530

    responsiveness improved

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,7 +6,7 @@ import { ScrollDown } from './ScrollDown';
 
 export const Home = () => {
   return (
-    <section className='home section' id='home'>
+    <section className='home sectionHome' id='home'>
         <div className='home__container container grid'>
             <div className='home__content grid'>
                 <Social/>
